a3338e941733c49006edde99829e8bfc05a80732
Arun Kadiveru <GGKTECH\arun.kadiveru@ggku2dell003.ggktech.local>
2019-03-08 22:36:07 +0530

    initial commit

---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,27 @@
+<section class="jumbotron text-center header">
+    <div class="container">
+        <h1 class="jumbotron-heading site-title">Perceptron Analytics</h1>
+        <p class="lead">Leader in Analytics.</p>
+        <div class="row menu-item">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Our Team</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+
+        </div>
+    </div>
+</section>
